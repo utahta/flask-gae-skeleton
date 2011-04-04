52aa9db06eafd123532b01ac13d14aa1c6263d78
Yuta <labs.ninxit@gmail.com>
2011-04-05 00:40:55 +0900

    update

--- copy_skeleton.py
+++ copy_skeleton.py
@@ -13,15 +13,9 @@ if __name__ == '__main__':
         sys.exit(1)
 
     to_dir = os.path.abspath(opt.to)
-    from_dir = os.path.dirname(os.path.abspath(__file__))
+    from_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app')
 
     print from_dir, ' to ', to_dir
-    shutil.copytree(from_dir, to_dir, symlinks=True)
-    os.remove(os.path.join(to_dir, 'copy_skeleton.py'))
-    try:
-        os.remove(os.path.join(to_dir, 'copy_skeleton.pyc'))
-    except:
-        pass
-    os.remove(os.path.join(to_dir, 'readme.md'))
+    shutil.copytree(from_dir, to_dir)
     os.chdir(to_dir)
     os.system('git remote rm origin')
